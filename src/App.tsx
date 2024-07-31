5b9e17c181960627eb5b6af4c88649e8dfd04234
trytofinderror <trytofinderror@gmail.com>
2024-07-31 20:22:01 +0300

    Headings

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
                 <Icon icon={"gem"} size={"7x"} spin={false}/>
             </div>
             <div className="p-4 text-eapo-blue-dark">
-                <Rainbow />
+                <Rainbow/>
             </div>
             <div className="p-4 text-eapo-red">
                 <Icon icon={"envelope-open"} size={"2xl"} spin={false}/>
@@ -32,9 +32,16 @@ function App() {
                 </FileLink>
             </div>
             <div className="p-4">
-                <Link href={"#"}>А это обычная ссылка</Link>
+                <Link href={"#"}>А это обычная ссылка, но она никуда не ведет</Link>
             </div>
-
+            <div className="p-4">
+                <h1>Заголовок 1</h1>
+                <h2>Заголовок 2</h2>
+                <h3>Заголовок 3</h3>
+                <h4>Заголовок 4</h4>
+                <h5>Заголовок 5</h5>
+                <h6>Заголовок 6</h6>
+            </div>
 
         </div>
     )
